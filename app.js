062f8b229bb0ce24fb0bc7774246dcfdefab8188
Shaun W Colley <swc7838@gmail.com>
2019-03-15 14:18:33 -0500

    fixed app.js away

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ fetch(HACKER_NEWS_URL)
 .then(function(response){
   return response.json()
 }).then(function(json){
-  let a = blah. map //for(let i = 0; i<json.length; i++){
+  for(let i = 0; i<json.length; i++){
   let NEWS_DETAILS_URL = `https://hacker-news.firebaseio.com/v0/item/${json[i]}.json?print=pretty`
   fetch(NEWS_DETAILS_URL)
   .then(function(response){
